cb3207b00d6f0f854993118989f5468020ed5831
Denis Palashesvkii <denis_palashevskii@epam.com>
2019-10-23 13:47:27 +0400

    remove log

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,55 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (env, argv) => {
-    console.log(argv.mode);
-    return {
-        entry: './src/main.js',
-        resolve: {
-            alias: {
-                svelte: path.resolve('node_modules', 'svelte')
-            },
-            extensions: ['.mjs', '.js', '.svelte'],
+module.exports = (env, argv) => ({
+    entry: './src/main.js',
+    resolve: {
+        alias: {
+            svelte: path.resolve('node_modules', 'svelte')
         },
-        output: {
-            filename: "bundle.js",
-            path: path.resolve(__dirname, 'public')
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.svelte$/,
-                    use: {
-                        loader: "svelte-loader",
-                        options: {
-                            emitCss: true,
-                            hotReload: true,
-                        }
+        extensions: ['.mjs', '.js', '.svelte'],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.svelte$/,
+                use: {
+                    loader: "svelte-loader",
+                    options: {
+                        emitCss: true,
+                        hotReload: true,
                     }
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        'css-loader',
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                config: {
-                                    ctx: {
-                                        env: argv.mode
-                                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            config: {
+                                ctx: {
+                                    env: argv.mode
                                 }
                             }
                         }
-                    ]
-                }
-            ]
-        },
-        devServer: {
-            contentBase: './public'
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: '[name].css'
-            })
-        ],
-    }
-};
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: './public'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+});
