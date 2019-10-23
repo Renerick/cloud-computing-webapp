b4362b27a13a338d5a5d68b9784ee49d19ecd7ef
Denis Palashesvkii <denis_palashevskii@epam.com>
2019-10-23 18:13:49 +0400

    fix styles

--- src/GroupListItem.svelte
+++ src/GroupListItem.svelte
@@ -3,7 +3,7 @@
     console.log(group);
 </script>
 
-<div class="p-4 bg-white hover:bg-active cursor-pointer border-l-2 border-primary-dark" class:bg-active={group.active}>
+<div class="p-4 hover:bg-active cursor-pointer border-l-2 border-primary-dark {group.active ? 'bg-active' : 'bg-white'}">
     <div class="flex justify-between items-baseline">
         <h2 class="text-xl">{group.name}</h2>
         <p class="text-primary-dark">{group.year}st year</p>
